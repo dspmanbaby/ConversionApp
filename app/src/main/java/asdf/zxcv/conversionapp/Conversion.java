58bb91c342118a6bed70eba7c29a95229ee4b5ad
NonChalant <32938970+dspmanbaby@users.noreply.github.com>
2019-12-14 20:42:53 -0800

    asdf

--- app/src/main/java/asdf/zxcv/conversionapp/Conversion.java
+++ app/src/main/java/asdf/zxcv/conversionapp/Conversion.java
@@ -1,4 +1,58 @@
 package asdf.zxcv.conversionapp;
 
 public class Conversion {
+
+    private final int FEET = 1;
+    private final double METERS_PER_FEET = 0.3048;
+    private final int INCHES = 2;
+    private final double CETIMETERS_PER_INCHES = 2.56;
+    private final int POUNDS = 3 ;
+    private final double GRAMS_PER_POUNDS = 453.592;
+
+    public Double input;
+    public Double output;
+    public int type; // what the user chooses (feet,inches,or pounds)
+
+    public String inputLabel;
+    public String outputLabel;
+
+    public Conversion() {
+        type = FEET;
+        inputLabel = "Feet";
+        outputLabel = "Meters";
+        input = 0.0;
+        output = 0.0;
+    }
+
+    public void switchToMeters() {
+        type = FEET;
+        inputLabel = "Feet";
+        outputLabel = "Meters";
+        convert();
+    }
+    public void switchToCentimeters() {
+        type = INCHES;
+        inputLabel = "Inches";
+        outputLabel = "Centimeters";
+        convert();
+    }
+    public void switchToGrams() {
+        type = POUNDS;
+        inputLabel = "Pounds";
+        outputLabel = "Grams";
+        convert();
+    }
+    public void convert() {
+        switch (type) {
+            case FEET:
+                output = input * METERS_PER_FEET;
+                break;
+            case INCHES:
+                output = input * CETIMETERS_PER_INCHES;
+                break;
+            case POUNDS:
+                output = input * GRAMS_PER_POUNDS;
+                break;
+        }
+    }
 }
--- app/src/main/java/asdf/zxcv/conversionapp/MainActivity.java
+++ app/src/main/java/asdf/zxcv/conversionapp/MainActivity.java
@@ -3,32 +3,98 @@ package asdf.zxcv.conversionapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
 import android.os.Bundle;
+import android.service.autofill.TextValueSanitizer;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Conversion conversion;
+    private TextView inputLabel;
+    private TextView outputLabel;
+    private EditText inputValue;
+    private TextView outputValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        conversion = new Conversion();
+        inputLabel = findViewById(R.id.textView);
+        inputLabel.setText(conversion.inputLabel);
+        outputLabel = findViewById(R.id.textView2);
+        outputLabel.setText(conversion.outputLabel);
+        outputValue = findViewById(R.id.textView3);
+        outputValue.setText(conversion.output.toString());
+        inputValue = findViewById(R.id.editText);
+        inputValue.setText(conversion.input.toString());
+        inputValue.addTextChangedListener(inputTextWatcher);
+    }
+    private TextWatcher inputTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            try {
+                conversion.input = Double.parseDouble(s.toString());
+            } catch (NumberFormatException e) {
+                conversion.input = 0.0;
+            }
+            conversion.convert();
+            outputValue.setText(conversion.output.toString());
+        }
+        @Override
+        public void afterTextChanged(Editable s) {
+        }
+    };
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if(event.getAction() == MotionEvent.ACTION_DOWN) {
+            toggleActionBar();
+        }
+         return true;
+    }
+
+    public void toggleActionBar() {
+        ActionBar actionBar = getActionBar();
+
+        if(actionBar !=null) {
+            if(actionBar.isShowing()){
+                actionBar.hide();
+            }
+        }
+        else{
+            actionBar.show();
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-       getMenuInflater().inflate(R.menu.app_menu,menu);
-       return true;
+        getMenuInflater().inflate(R.menu.app_menu, menu);
+        return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-       int id = item.getItemId();
-       switch (id){
-           case R.id.menuitem_feet_to_meter:
-
-           case R.id.menuitem_quit:
-               finish();
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.menuitem_feet_to_meters:
+                conversion.switchToMeters();
+            case R.id.menuitem_inches_to_centimeters:
+                conversion.switchToCentimeters();
+            case R.id.menuitem_pounds_to_grams:
+                conversion.switchToGrams();
+            case R.id.menuitem_quit:
+                finish();
+        }
+        return true;
     }
-    return true;
 }
